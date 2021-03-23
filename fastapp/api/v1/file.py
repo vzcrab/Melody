03721db2b97cfb39a8ba534f90f95f5b9da17d31
nKsnC <nksnc@qq.com>
2021-03-23 12:32:56 +0800

    perf: 响应模型与错误处理

--- fastapp/api/v1/file.py
+++ fastapp/api/v1/file.py
@@ -4,11 +4,11 @@
 import shutil
 import os
 
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, File, UploadFile, HTTPException, status
 
 from fastapp.common.logger import logger
 from fastapp.core.config import settings
-from fastapp.api.schemas.response import response_code
+from fastapp.api import schemas
 
 """
 文件操作路由
@@ -21,21 +21,17 @@ from fastapp.api.schemas.response import response_code
 router = APIRouter()
 
 
-@router.post('/uploadfile', summary='上传文件')
+@router.post('/uploadfile', summary='上传文件', response_model=schemas.Msg, responses={415: {}})
 async def create_upload_file(file: UploadFile = File(...)):
     # TODO 前端做hash, 将值上传, 判断
     # TODO 权限, 用户上传
-    # TODO 类型验证
-    # 类型验证, 可以用content_tpye 文件问类型, mime, 这种前端发包改不就行了, 没有什么意义
-    # 第二种, 由工具判别就行, 功能不能读肯定就会报错, 这个时候返回错误信息就行了
-    # 第三种, 使用第三方库判断
-    # 第四种, os库获取文件后缀, 进行判断
 
     logger.info(f"用户->上传文件:{file.filename}")
 
     file_ext = os.path.splitext(file.filename)[-1]
     if file_ext != 'apk' or file_ext != 'ipa':
-        return response_code.resp_415()
+        raise HTTPException(
+            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE, detail='不支持该媒体类型')
 
     uploadfile_path = os.path.join(settings.BASE_PATH, 'data', 'uploadfile')
     if not os.path.exists(uploadfile_path):
@@ -43,11 +39,7 @@ async def create_upload_file(file: UploadFile = File(...)):
 
     file_path = os.path.join(uploadfile_path, file.filename)
 
-    try:
-        with open(file_path, 'wb') as f:
-            c_path = shutil.copyfileobj(file.file, f)
-    except Exception as e:
-        logger.error(e)
-        return response_code.resp_500()
+    with open(file_path, 'wb') as f:
+        c_path = shutil.copyfileobj(file.file, f)
 
-    return response_code.resp_200()
+    return {"msg": "文件已上传"}
