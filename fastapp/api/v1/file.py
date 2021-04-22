663fbb8f035cff061767741c5ba0129b2d2b6e99
nKsnC <nksnc@qq.com>
2021-04-22 11:13:17 +0800

    refactor: 调整模型文件组织

--- fastapp/api/v1/file.py
+++ fastapp/api/v1/file.py
@@ -25,7 +25,6 @@ router = APIRouter()
 @router.post('/uploadfile', summary='上传文件', response_model=schemas.AppInfoModel, responses={415: {}})
 async def create_upload_file(file: UploadFile = File(...), user: schemas.User = Depends(deps.get_current_user)):
     # TODO 前端做hash, 将值上传, 判断
-    # TODO 权限, 用户上传
 
     logger.info(f"用户 {user} ->上传文件:{file.filename}")
 
@@ -43,6 +42,8 @@ async def create_upload_file(file: UploadFile = File(...), user: schemas.User = 
     with open(file_path, 'wb') as f:
         c_path = shutil.copyfileobj(file.file, f)
 
-    app_info = AppInfo(file_path).apk_parser()
+    app_info = AppInfo(file_path).apk_parser()  # TODO 分离出来
 
     return app_info
+
+# TODO 建立一个全局的websocket
