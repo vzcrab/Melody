e1cf168cda6f71865c221cfb160d9f679eec4ec4
nKsnC <nksnc@qq.com>
2021-04-23 19:38:12 +0800

    fix: win平台使用

--- plugins/DMA/decompile.py
+++ plugins/DMA/decompile.py
@@ -9,7 +9,8 @@ def path_to_dict(path):
     d = {'name': os.path.basename(path)}
     if os.path.isdir(path):
         d['type'] = "directory"
-        d['children'] = [path_to_dict(os.path.join(path, x)) for x in os.listdir(path)]
+        d['children'] = [path_to_dict(os.path.join(path, x))
+                         for x in os.listdir(path)]
     else:
         d['type'] = "file"
     return d
@@ -19,7 +20,7 @@ def decompile(path, out_path):
     system = platform.system()
     file_output = open("result", "w")
     if system == "Windows":
-        call("lib/jadx.bat --log-level error --deobf --deobf-parse-kotlin-metadata -d " + out_path + " " + path,
+        call(".\lib\jadx.bat --log-level error --deobf --deobf-parse-kotlin-metadata -d " + out_path + " " + path,
              shell=True,
              stdout=file_output)
     else:
@@ -35,3 +36,10 @@ def decompile(path, out_path):
         else:
             err = res.split("\n")
             return err
+
+
+if __name__ == '__main__':
+    out_path = r"/Users/ios/Downloads/out"
+    file_path = r"/Users/ios/Downloads/ReflectMaster-3.5.3.apk"
+    result = decompile(file_path, out_path)
+    print(result)
