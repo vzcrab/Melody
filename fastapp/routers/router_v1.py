b91f7913deb700f4e6cdb2201f05c2565b960bb1
nKsnC <nksnc@qq.com>
2021-05-08 08:52:22 +0800

    fix: 添加路由

--- fastapp/routers/router_v1.py
+++ fastapp/routers/router_v1.py
@@ -3,7 +3,7 @@
 
 from fastapi import APIRouter
 
-from fastapp.api.v1 import file, parser, ws
+from fastapp.api.v1 import file, ws, auth
 
 """
 版本路由区分与管理路由
@@ -16,5 +16,5 @@ from fastapp.api.v1 import file, parser, ws
 api_v1_router = APIRouter()
 
 api_v1_router.include_router(file.router)
-api_v1_router.include_router(parser.router)
 api_v1_router.include_router(ws.router)
+api_v1_router.include_router(auth.router)
